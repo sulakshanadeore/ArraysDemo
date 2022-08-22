ed6a673d5cc8ed7374acb70265b3e9c2d53acd7c
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-08-22 13:00:52 +0530

    generic demo

---
+++ ArraysDemo/GenericDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    class GenericDemo
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>();
+            list.Add(34);
+            list.Add(3555);
+            list.Add(21);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            dic.Add(1, "Hitendra");
+            dic.Add(2, "Mohan");
+            dic.Add(3, "Simran");
+            
+            foreach (KeyValuePair<int,string> item in dic)
+            {
+                Console.WriteLine(item.Key +" " + item.Value);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
